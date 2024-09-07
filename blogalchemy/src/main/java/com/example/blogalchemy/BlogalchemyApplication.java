d1cef276cbd743cf1353ab69edd38e6d6d3c07fa
Vidya Sagar Reddy Desu <d.vidya381@gmail.com>
2024-09-06 20:55:47 -0400

    Initial Commit

---
+++ blogalchemy/src/main/java/com/example/blogalchemy/BlogalchemyApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogalchemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogalchemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogalchemyApplication.class, args);
+	}
+
+}
---
+++ blogalchemy/src/test/java/com/example/blogalchemy/BlogalchemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogalchemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogalchemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
